6275e17660e2e3b22cb456ca65698ece0a9c1c88
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 18:30:52 +0300

    pythonnnnn

--- exercises/functions.py
+++ exercises/functions.py
@@ -1,4 +1,10 @@
 # Write your solution for 1.4 here!
 
-def is_prime(x):
-	pass
+def is_prime(num):
+	# if (x % 1 == 0) and (x % x == 0):
+		for i in range(num):
+			if (num % i != 0):
+				print("bm")
+
+
+is_prime(10)
--- exercises/superheros.py
+++ exercises/superheros.py
@@ -1,1 +1,15 @@
 # Write your solutions for 1.5 here!
+class Superheros:
+	def __init__(self, name, superpower,strength ):
+		self.name = name
+		self.superpower = superpower
+		self.strength = strength
+	def save_civilian (self, work):
+		strength = strength - work
+		if (work > strength):																						
+			print("Superhero is not strong enough! :(")
+
+super = Superheros("spiderman", "strings", "walls")
+print (super.name)
+print(super.superpower)
+
